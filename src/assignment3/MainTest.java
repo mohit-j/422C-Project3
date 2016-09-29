2d7174d57c703665a05868e1df08b6e31c5f54c7
mohit-j <mohit_joshi@Mohits-MacBook-Pro.local>
2016-09-28 22:40:48 -0500

    Fixed header and added test case

--- src/assignment3/MainTest.java
+++ src/assignment3/MainTest.java
@@ -4,18 +4,27 @@
 package assignment3;
 
 import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
-/**
- * @author mohit_joshi
- *
- */
 public class MainTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInit() {
+		Main.initialize();
+		assertTrue(Main.words != null && Main.bfsQueue != null && Main.markedWords != null && Main.neighbors != null);
+	}
+	
+	@Test
+	public void bfsNormalTest() {
+		Main.initialize();
+		ArrayList<String> test = new ArrayList<String>();
+		test.add("FLOOD"); test.add("BLOOD"); test.add("BROOD");
+		test.add("BROOS"); test.add("BROWS"); test.add("FROWS");
+		test.add("FRONS"); test.add("FRONT");
+		ArrayList<String> test2 = Main.getWordLadderBFS("FLOOD", "FRONT");
+		assertEquals(test, test2);
 	}
 
 }
--- src/assignment3/Node.java
+++ src/assignment3/Node.java
@@ -1,3 +1,16 @@
+/* WORD LADDER Node.java
+ * EE422C Project 3 submission by
+ * Mohit Joshi
+ * msj696
+ * #16475
+ * Ram Muthukumar
+ * rm48763
+ * #16470
+ * Slip days used: <0>
+ * Git URL: https://github.com/mohit-j/422C-Project3.git
+ * Fall 2016
+ */
+
 package assignment3;
 
 public class Node {
