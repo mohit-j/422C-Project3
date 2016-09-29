4f6f67434ba325b501bc255804509b760a1725dc
mohit-j <mohit_joshi@wireless-10-146-60-197.public.utexas.edu>
2016-09-28 20:07:48 -0500

    Added comments and created JUNIT file

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -24,6 +24,11 @@ public class Main {
 	public static Set<String> markedWords;
 	public static HashMap<String, HashSet<String>> neighbors;
 	
+	/**
+	 * This method connects the scanner to the keyboard and runs a test case of printLadder()
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 		
 		Scanner kb;	// input Scanner for commands
@@ -43,6 +48,9 @@ public class Main {
 		printLadder(getWordLadderBFS(input.get(0), input.get(1)));
 	}
 	
+	/**
+	 * This method instantiates the global variables and generates the list of neighbors for every word
+	 */
 	public static void initialize() {
 		
 		words = makeDictionary();
@@ -53,51 +61,68 @@ public class Main {
 	}
 	
 	/**
+	 * This method reads from the scanner and returns the first two words
 	 * @param keyboard Scanner connected to System.in
 	 * @return ArrayList of 2 Strings containing start word and end word. 
-	 * If command is /quit, return empty ArrayList. 
+	 * If command is /quit, exit the program
 	 */
 	public static ArrayList<String> parse(Scanner keyboard) {
 		
 		ArrayList<String> in = new ArrayList<String>();
 		do {
 			
-			in.add(keyboard.next().toUpperCase());
+			in.add(keyboard.next().toUpperCase());			//add input from console
 			if(in.get(0).equals("/QUIT") || (in.size()>1 && in.get(1).equals("/QUIT")))
-				System.exit(1);
+				System.exit(1);				//check if user wants to quit
 			
 		} while(in.size() < 2);
 		
 		return in;
 	}
 	
+	/**
+	 * This method creates a ladder connecting the start and end word using an DFS algorithm
+	 * @param start is the starting word
+	 * @param end is the goal word
+	 * @return ArrayList containing the ladder, or an empty ArrayList if a ladder does not exist
+	 */
 	public static ArrayList<String> getWordLadderDFS(String start, String end) {
 		
 		markedWords.clear();
 		Node bottom = depthFirstSearch(new Node(start), end);
 		ArrayList<String> ladder = new ArrayList<String>();
-		Node temp = bottom;
-		while(temp != null){
-			ladder.add(0, temp.word);
-			temp = temp.parent;
+		Node temp = bottom;					//start at ending word
+		while(temp != null){				
+			ladder.add(0, temp.word);		//add to ladder
+			temp = temp.parent;				//move on to the previous word
 		}
 		return shorten(ladder); 
 	}
 	
-    public static ArrayList<String> getWordLadderBFS(String start, String end) {
+    /**
+     * This method creates a ladder connecting the start and end word using an BFS algorithm
+     * @param start is the starting word
+	 * @param end is the goal word
+     * @return ArrayList containing the ladder, or an empty ArrayList if a ladder does not exist
+     */
+	public static ArrayList<String> getWordLadderBFS(String start, String end) {
 		
     	markedWords.clear();
     	bfsQueue.clear();
     	Node bottom = breadthFirstSearch(start, end);
 		ArrayList<String> ladder = new ArrayList<String>();
-		Node temp = bottom;
-		while(temp != null){
-			ladder.add(0, temp.word);
-			temp = temp.parent;
+		Node temp = bottom;					//start at ending word
+		while(temp != null){				
+			ladder.add(0, temp.word);		//add to ladder
+			temp = temp.parent;				//move on to the previous word
 		}
 		return ladder; 
 	}
     
+	/**
+	 * This method adds all the words from a text file into a set
+	 * @return Set containing all the words found in the text file
+	 */
 	public static Set<String>  makeDictionary () {
 		Set<String> words = new HashSet<String>();
 		Scanner infile = null;
@@ -114,13 +139,17 @@ public class Main {
 		return words;
 	}
 	
+	/**
+	 * This method prints a given ladder to the console
+	 * @param ladder is the ladder to be printed
+	 */
 	public static void printLadder(ArrayList<String> ladder) {
 		
-		if(ladder == null || ladder.isEmpty())
+		if(ladder == null || ladder.isEmpty())		//check if a ladder was found
 		{
 			System.out.println("no word ladder can be found between " + input.get(0).toLowerCase() + " and " + input.get(1).toLowerCase() + ".");
 		}
-		else
+		else										//print ladder if it exists
 		{
 			System.out.println("a " + (ladder.size()-2) + "-rung word ladder exists between " + ladder.get(0).toLowerCase() + " and " + ladder.get(ladder.size()-1).toLowerCase() + ".");
 			for(String s : ladder)
@@ -128,6 +157,9 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * This method generates a list of all words 1 letter away for every word in the dictionary
+	 */
 	public static void generateNeighbors() {
 		
 		if(neighbors!=null && !neighbors.isEmpty())
@@ -137,7 +169,7 @@ public class Main {
 		{
 		    HashSet<String> nlist = new HashSet<String>();
 		    for(int i=0; i<5; i++)
-		    {
+		    {									//test every possible word 1 letter away
 				String test = s;
 		    	for(int j=1; j<26; j++)
 		    	{
@@ -146,9 +178,9 @@ public class Main {
 		    		else
 		    			test = test.substring(0, i) + (char)(test.charAt(i)+1) + test.substring(i+1, s.length());
 		    		
-		    		if(words.contains(test))
+		    		if(words.contains(test))	//check if it's in the dictionary
 		    		{
-		    			nlist.add(test);
+		    			nlist.add(test);		//add it to list of neighbors
 		    		}
 		    	}
 		    }
@@ -157,7 +189,8 @@ public class Main {
 	}
 	
 	/**
-	 * This function performs the depth first search and returns a node containing the end word and a path back to the root
+	 * This function performs BFS and returns a node containing the end word and a path back to the root
+	 * @param start the node containing the start word
 	 * @param end the word to search for
 	 * @return node containing parent nodes back to root
 	 */
@@ -180,46 +213,57 @@ public class Main {
 		return null;
 	}
 	
+	/**
+	 * This function performs DFS and returns a node containing the end word and a path back to the root
+	 * @param start the node containing the start word
+	 * @param end the word to search for
+	 * @return node containing parent nodes back to root
+	 */
 	public static Node depthFirstSearch(Node start, String end) {
 		
 		markedWords.add(start.word);
-		
-		if(start.word.equals(end))
+														//base case
+		if(start.word.equals(end))						//check if we have reached the end word
 			return start;
-		else if(markedWords.size() == words.size())
+		else if(markedWords.size() == words.size())		//check if we have gone through every word
 			return null;
 		
-		for(String s : neighbors.get(start.word))
+		for(String s : neighbors.get(start.word))		//iterate through a word's neighbors
 			if(!markedWords.contains(s))
 			{
 				Node n = depthFirstSearch(new Node(s, start), end);
-				if(n == null)
+				if(n == null)							//recursively call itself with new start node
 					continue;
 				return n;
 			}
 		return null;
 	}
 	
+	/**
+	 * This method takes a ladder and attempts to shorten it by looking for if any element has a neighbor elsewhere in the ladder
+	 * @param list the ladder to be shortened
+	 * @return ArrayList containing the shortened ladder
+	 */
 	public static ArrayList<String> shorten(ArrayList<String> list) {
 		
 		int start = 0;
 		int end = 0;
 		
 		outerloop:
-		for(int j=0; j<list.size(); j++)
+		for(int j=0; j<list.size(); j++)			//iterate starting from front
 		{
-			for(int k=list.size()-1; k>j; k--)
+			for(int k=list.size()-1; k>j; k--)		//iterate starting from back
 			{
 				if(neighbors.get(list.get(j)).contains(list.get(k)) || list.get(j).equals(list.get(k)))
 				{
-					start = j+1;
+					start = j+1;					//check if the two words are neighbors
 					end = k;
 					break outerloop;
 				}
 			}
 		}
 		
-		list.subList(start, end).clear();
+		list.subList(start, end).clear();			//remove everything in between
 		
 		return list;
 	}
---
+++ src/assignment3/MainTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package assignment3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author mohit_joshi
+ *
+ */
+public class MainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
